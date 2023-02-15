2102800ee870a52dbfa6ab2a32ccb0269bf5e503
Dennis Hedback <d.hedback@gmail.com>
2023-02-15 09:50:52 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 
-import os
+from pathlib import Path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+THIS_DIR = Path(__file__).parent
+
+
+def get_requirements():
+    requirements_path = THIS_DIR / "requirements.txt"
+    with open(requirements_path) as f:
+        requirements = f.read().splitlines()
+    return requirements
+
 
 setup(
     name="benkpress",
     version="0.1.2",
     description="PyQt/PDFJs GUI for creating and evaluating classifiers for PDF"
     "file/page/sentence targets using sklearn compatible pipelines",
-    long_description=open(os.path.join(THIS_DIR, "README.md")).read(),
+    long_description=open(THIS_DIR / "README.md").read(),
     long_description_content_type="text/markdown",
     author="Dennis Hedback",
     author_email="d.hedback@gmail.com",
     url="https://github.com/dennishedback/benkpress",
     packages=find_packages(),
-    install_requires=[
-        "pandas>=1.4.2",
-        "numpy>=1.22.4",
-        "pdf2image",
-        "pytesseract",
-        "PyQt6",
-        "PyQt6-WebEngine",
-        "PyPDF2<=2.12.1",
-        "scikit-learn",
-        "docopt",
-        "appdirs",
-    ],
+    install_requires=get_requirements(),
     entry_points={
         "console_scripts": [
             "benkpress=benkpress.application:main",
